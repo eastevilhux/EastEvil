98ee69f15f2f3f0bfeb037f5ac2350095dc5871b
eastevilhux@163.com <eastevilhux@163.com>
2020-09-08 21:50:17 +0800

    commit by myself

--- lib-uikit/src/main/java/com/god/uikit/widget/TabmenuLayout.kt
+++ lib-uikit/src/main/java/com/god/uikit/widget/TabmenuLayout.kt
@@ -1,9 +1,11 @@
 package com.god.uikit.widget
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Color
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.FrameLayout
 import androidx.annotation.Nullable
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -20,17 +22,21 @@ class TabmenuLayout : FrameLayout{
         const val LINE_WIDTH_DEFAULT = 50;
         const val LINE_HEIGHT_DEFAULT = 2;
         const val MARGIN_BOTTOM_DEFAULT = 5;
+        const val GONE = 1;
+        const val INVISIBLE = 2;
     }
 
     private var dataBinding : LayoutTabmenuBinding? = null;
 
     private val menuText = ObservableField<String>();
-    private val lineState = ObservableField<Boolean>();
+    private var lineState : Boolean = true;
     private var lineColor : Int = Color.WHITE;
     private var menuColor : Int = Color.WHITE;
     private var lineWidth : Int = ConstraintLayout.LayoutParams.WRAP_CONTENT;
     private var lineHeight : Int = ConstraintLayout.LayoutParams.WRAP_CONTENT;
     private var lineMarginBottom : Int = MARGIN_BOTTOM_DEFAULT.dip2Px()
+
+    private var hiddenType : Int = 1;
 
     constructor(context: Context) : super(context) {
 
@@ -55,27 +61,39 @@ class TabmenuLayout : FrameLayout{
     private fun initAttr(@Nullable attrs : AttributeSet){
         val ta = context.obtainStyledAttributes(attrs, R.styleable.TabmenuLayout);
         menuText.set(ta.getString(R.styleable.TabmenuLayout_menuText));
-        lineState.set(ta.getBoolean(R.styleable.TabmenuLayout_lineState,true));
+        lineState = ta.getBoolean(R.styleable.TabmenuLayout_lineState,true);
         menuColor = ta.getColor(R.styleable.TabmenuLayout_menuTextColor,Color.WHITE);
         lineColor = ta.getColor(R.styleable.TabmenuLayout_lineColor, Color.WHITE);
         lineWidth = ta.getDimensionPixelOffset(R.styleable.TabmenuLayout_line_width,LINE_WIDTH_DEFAULT.dip2Px());
         lineHeight = ta.getDimensionPixelOffset(R.styleable.TabmenuLayout_line_height,LINE_HEIGHT_DEFAULT.dip2Px());
         lineMarginBottom = ta.getDimensionPixelOffset(R.styleable.TabmenuLayout_lineMarginButtom,
             MARGIN_BOTTOM_DEFAULT.dip2Px());
+        hiddenType = ta.getIndex(R.styleable.TabmenuLayout_hiddenType);
         ta.recycle();
         init();
     }
 
+    @SuppressLint("WrongConstant")
     private fun init(){
         dataBinding = DataBindingUtil.inflate(
             LayoutInflater.from(context), R.layout.layout_tabmenu,
             this, true
         )
         dataBinding?.menuText = menuText;
-        dataBinding?.lineState = lineState;
 
         dataBinding?.tvItemtext!!.setTextColor(menuColor);
         dataBinding?.tvLine!!.setBackgroundColor(lineColor);
+
+        if(lineState){
+            //需要显示
+            if(hiddenType == GONE){
+                dataBinding?.tvLine!!.visibility = View.GONE;
+            }else{
+                dataBinding?.tvLine!!.visibility = INVISIBLE;
+            }
+        }else{
+            dataBinding?.tvLine!!.visibility = View.VISIBLE;
+        }
 
         var lineParams = dataBinding?.tvLine!!.layoutParams as MarginLayoutParams;
         lineParams.width = lineWidth;
@@ -85,12 +103,23 @@ class TabmenuLayout : FrameLayout{
     }
 
 
+    @SuppressLint("WrongConstant")
     fun setLineState(lineState:Boolean){
-        this.lineState.set(lineState);
+        this.lineState = lineState;
+        if(lineState){
+            //需要显示
+            if(hiddenType == GONE){
+                dataBinding?.tvLine!!.visibility = View.GONE;
+            }else{
+                dataBinding?.tvLine!!.visibility = INVISIBLE;
+            }
+        }else{
+            dataBinding?.tvLine!!.visibility = View.VISIBLE;
+        }
     }
 
     fun getLineState():Boolean{
-        return lineState.get() ?: false;
+        return lineState;
     }
 
     fun setMenuTextColor(color:Int){
@@ -100,6 +129,5 @@ class TabmenuLayout : FrameLayout{
     fun setMenuText(menuText : String){
         this.menuText.set(menuText);
     }
-
 
 }
